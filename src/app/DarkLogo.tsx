1ffe619f9533c27a0f32845baa38f02c0dd9884c
zerdoxEXP <keller@zerdox.de>
2023-12-15 09:55:10 +0100

    Typescript Error fix

--- src/app/DarkLogo.tsx
+++ src/app/DarkLogo.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-const DarkLogo = (props) => (
+const DarkLogo: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     xmlSpace="preserve"
--- src/app/LightLogo.tsx
+++ src/app/LightLogo.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-const LightLogo = (props) => (
+const LightLogo: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     xmlSpace="preserve"
