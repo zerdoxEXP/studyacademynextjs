920b95947a45a9b16325107c0e48bcaac9de6e08
zerdoxEXP <keller@zerdox.de>
2024-02-13 20:52:15 +0100

    2 Schritte

--- src/app/partnerprogramm/FeaturesHero.tsx
+++ src/app/partnerprogramm/FeaturesHero.tsx
@@ -3,7 +3,7 @@ export default function InhaltIntro() {
       <div className="px-6 pt-16 sm:pt-20 lg:px-8">
         <div className="mx-auto max-w-3xl text-left sm:text-center">
           <p className="font-caveat text-2xl sm:text-4xl font-medium leading-7 text-blue-600">Interesse? So kannst du...</p>
-          <h2 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-5xl">Affiliate werden in 3 Schritten</h2>
+          <h2 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-5xl">Affiliate werden in 2 Schritten</h2>
           <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-400">
             Du bekommst einen individuellen Affiliate-Link von unserem Bezahldienstleister CopeCart und wenn jemand über deinen Link kauft erhältst du automatisch deine Provision.
           </p>
